a957e9398906d2016b374003430cbd3474f6884e
harshay560 <harshaynarayan494@gmail.com>
2025-03-21 01:34:25 +0530

    first commit

---
+++ src/server.ts
@@ -0,0 +1,47 @@
+import express from "express";
+import { createServer } from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const server = createServer(app);
+const port = process.env.PORT || 3001;
+
+console.log(__dirname, port);
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("✅ WebSocket Server is Running!");
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+const users: Record<string, string> = {};
+
+io.on("connection", (socket) => {
+  socket.on("authenticate", ({ userId }) => {
+    console.log("Authenticated " + userId);
+    socket.join(userId);
+  });
+  socket.on("private_chat", ({ senderId, receiverId, message }) => {
+    console.log(senderId, receiverId, message);
+    io.to(receiverId).emit("private_chat", { senderId, message });
+  });
+});
+
+console.log(users);
+
+server.listen(port, () => console.log(`Example app listening on port ${port}`));
+
+export { io };
